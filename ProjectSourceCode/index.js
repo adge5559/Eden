948a88d2ecbade9b520d7f9284a7d0d616cdb1f5
Ella Larson <ella1662@colorado.edu>
2024-11-02 15:59:33 -0600

    fixing files again

---
+++ ProjectSourceCode/index.js
@@ -0,0 +1,38 @@
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcryptjs'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+//paths
+
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
