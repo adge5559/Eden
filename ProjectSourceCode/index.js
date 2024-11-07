6b7d1b6121f0cf8963e9c800870a59c554651ce4
Alex <alca1296@colorado.edu>
2024-11-07 10:09:30 -0700

    added testing infrastructure

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -34,5 +34,9 @@ app.get('/', (req, res) => {
     res.render('pages/home');
 });
 
-app.listen(3000);
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
+
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
