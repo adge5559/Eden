376a77f4f739ac3042053a1d30133e9dffdc1051
Ella Larson <ella1662@colorado.edu>
2024-11-26 10:44:25 -0700

    disk issues

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -462,7 +462,7 @@ app.get('/upload', (req, res) => {
 // Save posts
 app.post('/create-post', async (req, res) => {
   const form = new IncomingForm();
-  form.uploadDir = path.join(__dirname, '../mnt/uploads/images');
+  form.uploadDir = path.join(__dirname, '/var/data');
   form.keepExtensions = true;
 
   form.parse(req, async (err, fields, files) => {
